57b32b25a3c2db4f9c2dba35ab073e1440c1f2c9
wahyuamirulloh <amirullohw@gmail.com>
2020-12-14 15:18:01 +0700

    transition sidebar

--- sidebar/script.js
+++ sidebar/script.js
@@ -1,11 +1,11 @@
 document.getElementById('buttonsidebarclose').addEventListener('click', () => {
-    document.getElementById('sidebar').style.display = 'none';
+    document.getElementById('sidebar').style.left = '-200px';
     document.getElementById('buttonsidebaropen').style.display = 'block';
     console.log('Close');
 });
 
 document.getElementById('buttonsidebaropen').addEventListener('click', () => {
-    document.getElementById('sidebar').style.display = 'block';
+    document.getElementById('sidebar').style.left = '0';
     document.getElementById('buttonsidebaropen').style.display = 'none';
     console.log('Open');
 });
