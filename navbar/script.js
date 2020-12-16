03afbb155515be01c9c769d85182e18ddc980e4a
Wahyu Amirulloh <amirullohw@gmail.com>
2020-12-16 22:12:56 +0700

    Developing, completed

---
+++ navbar/script.js
@@ -0,0 +1,7 @@
+document.getElementById('navlink item-d').addEventListener('click', () => {
+    if(document.getElementById('navlink-item-d').style.display == 'none') {
+        document.getElementById('navlink-item-d').style.display = 'block';
+    } else {
+        document.getElementById('navlink-item-d').style.display = 'none';
+    }
+});
--- sidebar/README.md
+++ sidebar/README.md
@@ -5,6 +5,10 @@ X Styling <br/>
 X Responsive <br/>
 X Animation <br/>
 
+--- Compatibility Check --- <br/>
+O Linux - Firefox <br/>
+O Windows 10 - Edge <br/>
+
 #### About Author 
 Name : Wahyu Amirulloh <br/>
 GitHub : https://github.com/wahyuamirulloh <br/>
