c10c36cf3ec4da2a17541cc1490001a9197758c3
Wahyu Amirulloh <amirullohw@gmail.com>
2020-12-17 21:24:11 +0700

    add dropdown

---
+++ navbar/README.md
@@ -0,0 +1,16 @@
+# Navigation Bar with HTML, CSS and Javascript
+
+--- Status --- <br/>
+X Styling <br/>
+X Responsive <br/>
+X Animation <br/>
+
+--- Compatibility Check --- <br/>
+O Linux - Firefox <br/>
+O Windows 10 - Edge <br/>
+
+#### About Author 
+Name : Wahyu Amirulloh <br/>
+GitHub : https://github.com/wahyuamirulloh <br/>
+Facebook : Wahyu Amirulloh <br/>
+Twitter : @wahyu-amirulloh <br/>
--- sidebar/script.js
+++ sidebar/script.js
@@ -1,5 +1,5 @@
 document.getElementById('buttonsidebarclose').addEventListener('click', () => {
-    document.getElementById('sidebar').style.left = '-200px';
+    document.getElementById('sidebar').style.left = '-450px';
     document.getElementById('buttonsidebaropen').style.display = 'block';
     console.log('Close');
 });
@@ -8,6 +8,15 @@ document.getElementById('buttonsidebaropen').addEventListener('click', () => {
     document.getElementById('sidebar').style.left = '0';
     document.getElementById('buttonsidebaropen').style.display = 'none';
     console.log('Open');
+});
+
+document.getElementById('sidebar__item-4').addEventListener('click', () => {
+    console.log(document.getElementById('dropdown__item-4').style.display);
+    if(document.getElementById('dropdown__item-4').style.display == 'none') {
+        document.getElementById('dropdown__item-4').style.display = 'block';
+    } else {
+        document.getElementById('dropdown__item-4').style.display = 'none';
+    }
 });
 
 console.log('Javascript Successfully loaded');
