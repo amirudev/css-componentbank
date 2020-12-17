2e581181f671e4ddf68f9ddf70d4a273b0349891
Wahyu Amirulloh <amirullohw@gmail.com>
2020-12-17 22:04:41 +0700

    backup development - slideshow 2204 17122020

---
+++ slideshow-gallery/script.js
@@ -0,0 +1,17 @@
+const imageArray = [
+    'images/dummyimages1.png',
+    'images/dummyimages2.png',
+    'images/dummyimages3.png'
+]
+
+var currentImages = imageArray[0];
+
+document.getElementById('previmage').addEventListener('click', () => {
+
+});
+
+document.getElementById('previmage').addEventListener('click', () => {
+
+});
+
+document.getElementById('image-container').src = currentImages;
